e50804473b6b4ae18652578e959355aeda51ea61
Shivaram <shivaram1216@gmail.com>
2025-01-02 11:45:14 +0530

    Login controller create for admin side login

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function index() 
+    {
+        return view('adminlogin');
+    }
+
+    public function checkLogin(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        $admin = Admin::where('email', $validated['email'])->first();
+
+        if(!$admin || Hash::check($validated['password'], $admin->password)) {
+            return back()->withErrors(["email" => 'Invalid Email or Password']);
+        }
+
+        return redirect()->route('dashboard');
+    }
+}
