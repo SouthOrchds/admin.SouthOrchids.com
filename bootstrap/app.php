117251991339d1da3318cf80ba272e55dec4ab01
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-03-04 09:39:59 +0530

    Middleware update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(HandleCors::class);
+
+        $middleware->alias([
+            'check_permission' => App\Http\Middleware\CheckAdminPermission::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
