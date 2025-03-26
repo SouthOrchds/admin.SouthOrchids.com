13faf1a34ca5704618134dcc3a881095a0dced4e
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-03-26 09:37:14 +0530

    Need changes in those commites

--- resources/views/pages/adminlogin.blade.php
+++ resources/views/pages/adminlogin.blade.php
@@ -7,20 +7,22 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    @if($errors->any())
+    <div class="absolute inset-x-0 text-center bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded max-sm:text-start" role="alert">
+        <strong class="font-bold">Whoops!</strong>
+        {{-- <span class="block sm:inline max-sm:text-sm">Something went wrong:</span> --}}
+        <ul class="mt-2 ml-4 list-disc list-inside  max-sm:text-sm max-sm:mt-0 max-sm:ml-0">
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     <div class="flex justify-center items-center h-screen md:p-16 md:items-start max-sm:p-10 max-sm:items-start ">
         <div class="p-10 border w-96 shadow-md md:w-full max-sm:w-full ">
-            @if($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
             <p class="text-2xl font-medium text-center md:text-3xl">Login Page</p>
             <div class="py-10 flex justify-center">
-                <form action="{{ route('adminCheck') }}" method="POST">
+                <form action="{{ route('admin.check') }}" method="POST">
                     @csrf
                     <div class="py-1 md:py-5">
                         <p class="pb-1 text-lg md:text-xl">Email:</p>
